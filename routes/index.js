ca2e911a2a1f8da93b415d8e6648837684f7e07b
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-07-20 02:39:41 -0400

    init home route logic

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 const articleController = require('../controllers/articleController');
+const Article = require('../models/article');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({
-    message: 'Welcome to the homepage'
-  });
+  async.parallel(
+    {
+      articles: function(callback) {
+        Article.find({}, callback);
+      }
+    },
+    function(err, results) {
+      res.json({
+        title: 'Home',
+        articles: results,
+        error: err,
+      });
+    }
+  );
 });
 
 module.exports = router;
