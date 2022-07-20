7c1c4a1a71fc3c04904f3d18315999628eee650a
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-07-20 02:40:20 -0400

    added multer middleware to blogs/create route

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController');
 const commentController = require('../controllers/commentController');
+const multer  = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 /* article routes */
 router.get('/', articleController.article_list_get);
 
-router.post('/', articleController.article_form_post);
+router.post('/create', upload.single("image"), articleController.article_form_post);
 
 router.get('/create', articleController.article_form_get);
 
