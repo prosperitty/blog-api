65c748340f223134633c2127447de6bf296db0a7
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-08-08 02:21:33 -0400

    added comment form GET and POST logic

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,3 +1,7 @@
+const Comment = require('../models/comment');
+var async = require('async');
+const { body, validationResult } = require('express-validator');
+
 exports.comment_list_get = function (req, res, next) {
   res.json({message: 'GET comment list'});
 }
@@ -7,12 +11,45 @@ exports.comment_get = function (req, res, next) {
 }
 
 exports.comment_form_get = function (req, res, next) {
-  res.json({message: 'GET comment form'});
+  res.json({header: 'Create new comment'});
 }
 
-exports.comment_form_post = function (req, res, next) {
-  res.json({message: 'POST comment form'});
-}
+exports.comment_form_post = [
+  //validation and sanitization.
+  body('comment', 'comment required')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  //Process request after validation and sanitization.
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    //create new Developer
+    var comment = new Comment({
+      comment: req.body.comment,
+      article: req.params.articleid,
+    });
+
+    if (!errors.isEmpty()) {
+      res.json({
+        comment: comment,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      //data is valid
+      comment.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        //success
+        console.log('New Comment ' + comment);
+        res.redirect('/blogs/' + comment.article._id);
+      });
+    }
+  },
+];
 
 exports.comment_form_put = function (req,res, next) {
   res.json({message: 'PUT comment form'});
