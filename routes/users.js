df9d6189ac9d91b33f0b802e510498b2914b9f02
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-08-12 00:52:20 -0400

    added log out and authentication route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
+const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.json({
-    message: 'Welcome to the users route',
-  });
+/* GET user profile. */
+router.get('/profile',function(req, res, next) {
+    res.json({message: 'profile route'});
 });
+
+router.get('/authenticated', userController.users_isLoggedIn);
+
+router.get('/logout', userController.users_logout_post);
 
 module.exports = router;
