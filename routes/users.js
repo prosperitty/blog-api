61c6ffbd5651bd59ab5ffa0c05e3d885227816aa
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-11-20 02:48:32 -0500

    added queries for published and unpublished articles

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+const Article = require('../models/article');
 const userController = require('../controllers/userController');
 
-router.get('/profile',function(req, res, next) {
-    res.json({message: 'profile route'});
+router.get('/profile', function (req, res, next) {
+  async.parallel(
+    {
+      published_articles: function (callback) {
+        Article.find({ user: req.user._id, isPublished: true })
+          .populate('comments')
+          .populate('category')
+          .exec(callback);
+      },
+      unpublished_articles: function (callback) {
+        Article.find({ user: req.user._id, isPublished: false })
+          .populate('comments')
+          .populate('category')
+          .exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      console.log(req.user);
+      res.json({
+        unpublished_articles: results.unpublished_articles,
+        published_articles: results.published_articles,
+        user: req.user,
+        error: err,
+      });
+    }
+  );
+
+  // res.json({message: 'profile route'});
 });
 
 router.get('/authenticated', userController.users_isLoggedIn);
