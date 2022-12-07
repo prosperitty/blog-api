e22d3669820583ded673c65d1b3784c4011c65e7
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-07 03:46:50 -0500

    queried relevant data for home page

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,70 @@
 var express = require('express');
 var router = express.Router();
 var async = require('async');
-const articleController = require('../controllers/articleController');
 const Article = require('../models/article');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  async.parallel(
-    {
-      articles: function(callback) {
-        Article.find({}, callback);
-      }
+router.get('/', function (req, res, next) {
+  // Article.count().exec(function (err, count) {
+
+  //   // Get a random entry
+  //   var random = Math.floor(Math.random() * count)
+
+  //   // Again query all users but only fetch one offset by our random #
+  //   Article.findOne().skip(random).exec(
+  //     function (err, result) {
+  //       // Tada! random user
+  //       console.log(result)
+  //     })
+  // })
+  async.parallel({
+    mainArticle: function(callback) {
+        Article.countDocuments()
+        .exec(function (err, count) {
+          // Get a random entry
+          var random = Math.floor(Math.random() * count)
+          // Again query all users but only fetch one offset by our random #
+          Article.findOne().skip(random).populate('user').populate('category').exec(callback)
+        })
     },
-    function(err, results) {
+    //bug after this query. get _id for categories instead of string
+    businessArticle: function(callback) {
+      Article.findOne({category: '63433c0bf956b9ec2934ecdc', isPublished: true})
+      .populate('category')
+      .exec(callback)
+    },
+    politicsArticle: function(callback) {
+      Article.findOne({category: '633d2bd499336cb4c9381d17', isPublished: true})
+      .populate('category')
+      .exec(callback)
+    },
+    scienceArticle: function(callback) {
+      Article.findOne({category: '63433c35f956b9ec2934ecf8', isPublished: true})
+      .populate('category')
+      .exec(callback)
+    },
+    healthArticle: function(callback) {
+      Article.findOne({category: '63433c39f956b9ec2934ed06', isPublished: true})
+      .populate('category')
+      .exec(callback)
+    },
+    technologyArticle: function(callback) {
+      Article.findOne({category: '63433c30f956b9ec2934ecea', isPublished: true})
+      .populate('category')
+      .exec(callback)
+    },
+
+  },
+    function (err, results) {
+      console.log(err)
       res.json({
         title: 'Home',
-        articles: results,
+        main_article: results.mainArticle,
+        business_article: results.businessArticle,
+        politics_article: results.politicsArticle,
+        science_article: results.scienceArticle,
+        health_article: results.healthArticle,
+        technology_article: results.technologyArticle,
         error: err,
       });
     }
