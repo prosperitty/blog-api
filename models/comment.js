d0cfeef123cce4c9c0d4ed8b42fc38a5ea95b4f0
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-07 03:45:40 -0500

    adjusted formatted date virtual schema

--- models/comment.js
+++ models/comment.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: { virtuals: true } };
 const commentSchema = new Schema(
   {
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     date: {type: Date, default: Date.now},
     comment: {type: String, required: true},
     article: {type: Schema.Types.ObjectId, ref: 'Article', required: true},
-  }
+  }, opts
 );
 
 //fix route
@@ -17,6 +18,18 @@ commentSchema
 .get(function () {
   return '/blogs/' + this.article._id + '/comments/' + this._id;
 });
+
+commentSchema
+.virtual('date_formatted')
+.get(function () {
+  const options = {
+    year: "2-digit",
+    month: "short", 
+    day: "numeric",
+  }
+  return this.date.toLocaleDateString("en-US", options);
+});
+
 
 // commentSchema
 // .virtual('formUrl')
