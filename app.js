864df16a7d9f7069699508c48da9c8465242ca3d
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-09 02:48:52 -0500

    updated origin route

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ db.on('error', console.error.bind(console, 'MONGODB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors({ origin: "https://alex-lvl.github.io/blog-react/", credentials: true}));
+app.use(cors({ origin: "https://alex-lvl.github.io", credentials: true}));
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
