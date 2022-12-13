e6b9786430a3f3fb173d79a6d9147b79403f69dc
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-13 02:33:01 -0500

    specified new origin response header route

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,35 +2,17 @@ var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController');
 const commentController = require('../controllers/commentController');
-const multer = require('multer');
+const multer  = require('multer')
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // article routes
 
-app.get(
-  '/users/:userId',
-  cors({
-    origin: 'https://example.com/users/:userId',
-  }),
-  (req, res) => {
-    // Return the user with the specified userId
-  }
-);
-
-router.get(
-  '/',
-  cors({ origin: 'https://alex-lvl.github.io/blogs' }),
-  articleController.article_list_get
-);
+router.get('/', articleController.article_list_get);
 
 router.get('/create', articleController.article_form_get);
 
-router.post(
-  '/create',
-  upload.single('image'),
-  articleController.article_form_post
-);
+router.post('/create', upload.single("image"), articleController.article_form_post);
 
 router.get('/:articleid', articleController.article_get);
 
@@ -47,14 +29,8 @@ router.post('/:articleid/comments/create', commentController.comment_form_post);
 
 router.get('/:articleid/comments/:commentid', commentController.comment_get);
 
-router.put(
-  '/:articleid/comments/:commentid',
-  commentController.comment_form_put
-);
+router.put('/:articleid/comments/:commentid', commentController.comment_form_put);
 
-router.delete(
-  '/:articleid/comments/:commentid',
-  commentController.comment_form_delete
-);
+router.delete('/:articleid/comments/:commentid', commentController.comment_form_delete);
 
 module.exports = router;
