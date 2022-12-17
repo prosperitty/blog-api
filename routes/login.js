538f33a99aa50a02fd833c41cdcb1b17d89abc3b
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-17 04:11:15 -0500

    added res.cookie

--- routes/login.js
+++ routes/login.js
@@ -60,6 +60,14 @@ router.post(
             console.log(err, 'login user error')
             return next(err);
            }
+           res.cookie('session', req.sessionID, {
+            maxAge: 86400000, // 1 day in milliseconds
+            secure: true,
+            httpOnly: true,
+            domain: 'alex-lvl.github.io',
+            path: '/',
+            sameSite: 'None'
+          });
            console.log('successful login')
            console.log(user);
           return res.redirect("https://alex-lvl.github.io/blog-react");
