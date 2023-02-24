ec5f894740b7c9a7568b7dc319fc8ce33fb6eeb1
Alex Marmolejo <alex97marmolejo@gmail.com>
2023-02-24 01:25:48 -0500

    increased performance of querying for random main article

--- routes/index.js
+++ routes/index.js
@@ -5,66 +5,100 @@ const Article = require('../models/article');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // Article.count().exec(function (err, count) {
-
-  //   // Get a random entry
-  //   var random = Math.floor(Math.random() * count)
-
-  //   // Again query all users but only fetch one offset by our random #
-  //   Article.findOne().skip(random).exec(
-  //     function (err, result) {
-  //       // Tada! random user
-  //       console.log(result)
-  //     })
-  // })
-  async.parallel({
-    mainArticle: function(callback) {
-        Article.countDocuments()
-        .exec(function (err, count) {
-          // Get a random entry
-          var random = Math.floor(Math.random() * count)
-          // Again query all users but only fetch one offset by our random #
-          Article.findOne({isPublished: true}).skip(random).populate('user').populate('category').exec(callback)
+  async.parallel(
+    {
+      mainArticle: function (callback) {
+        Article.aggregate([
+          { $match: { isPublished: true } },
+          { $sample: { size: 1 } },
+          {
+            $addFields: {
+              date_formatted: {
+                $dateToString: { format: '%M-%d-%Y', date: '$date' },
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'user',
+              foreignField: '_id',
+              as: 'user',
+            },
+          },
+          {
+            $lookup: {
+              from: 'categories',
+              localField: 'category',
+              foreignField: '_id',
+              as: 'category',
+            },
+          },
+          {
+            $unwind: '$category',
+          },
+          {
+            $unwind: '$user',
+          },
+        ]).exec(callback);
+      },
+      //bug after this query. get _id for categories instead of string
+      businessArticle: function (callback) {
+        Article.findOne({
+          category: '63433c0bf956b9ec2934ecdc',
+          isPublished: true,
         })
+          .select('-image')
+          .sort({ date: -1 })
+          .populate('category')
+          .exec(callback);
+      },
+      politicsArticle: function (callback) {
+        Article.findOne({
+          category: '633d2bd499336cb4c9381d17',
+          isPublished: true,
+        })
+          .select('-image')
+          .sort({ date: -1 })
+          .populate('category')
+          .exec(callback);
+      },
+      scienceArticle: function (callback) {
+        Article.findOne({
+          category: '63433c35f956b9ec2934ecf8',
+          isPublished: true,
+        })
+          .select('-image')
+          .sort({ date: -1 })
+          .populate('category')
+          .exec(callback);
+      },
+      healthArticle: function (callback) {
+        Article.findOne({
+          category: '63433c39f956b9ec2934ed06',
+          isPublished: true,
+        })
+          .select('-image')
+          .sort({ date: -1 })
+          .populate('category')
+          .exec(callback);
+      },
+      technologyArticle: function (callback) {
+        Article.findOne({
+          category: '63433c30f956b9ec2934ecea',
+          isPublished: true,
+        })
+          .select('-image')
+          .sort({ date: -1 })
+          .populate('category')
+          .exec(callback);
+      },
     },
-    //bug after this query. get _id for categories instead of string
-    businessArticle: function(callback) {
-      Article.findOne({category: '63433c0bf956b9ec2934ecdc', isPublished: true})
-      .select('-image')
-      .populate('category')
-      .exec(callback)
-    },
-    politicsArticle: function(callback) {
-      Article.findOne({category: '633d2bd499336cb4c9381d17', isPublished: true})
-      .select('-image')
-      .populate('category')
-      .exec(callback)
-    },
-    scienceArticle: function(callback) {
-      Article.findOne({category: '63433c35f956b9ec2934ecf8', isPublished: true})
-      .select('-image')
-      .populate('category')
-      .exec(callback)
-    },
-    healthArticle: function(callback) {
-      Article.findOne({category: '63433c39f956b9ec2934ed06', isPublished: true})
-      .select('-image')
-      .populate('category')
-      .exec(callback)
-    },
-    technologyArticle: function(callback) {
-      Article.findOne({category: '63433c30f956b9ec2934ecea', isPublished: true})
-      .select('-image')
-      .populate('category')
-      .exec(callback)
-    },
-
-  },
     function (err, results) {
-      console.log(err)
+      console.log(err);
       res.json({
         title: 'Home',
-        main_article: results.mainArticle,
+        main_article: results.mainArticle[0],
         business_article: results.businessArticle,
         politics_article: results.politicsArticle,
         science_article: results.scienceArticle,
